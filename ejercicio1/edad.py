1aa5cf48d1f15e90429eb046d501f0aa5bd404b6
JuanDiaz456 <juandiaz1@colegioguanenta.edu.co>
2022-04-29 07:46:42 -0500

    Desarrollo: diagrama y programa realizado

---
+++ ejercicio1/edad.py
@@ -0,0 +1,17 @@
+"""Ejercicio 1:
+programa paraverificar si una persona 
+es mayor de edad"""
+
+print("-------------------------")
+print("----- MAYOR DE EDAD -----")
+print("-------------------------")
+
+#INPUT
+edad= int(input("digite su edad: "))
+
+#PROCESSING
+if edad>=18:
+    print("la persona es mayor de edad")
+else:
+    print("la persona es menor de edad")
+
